2d0a544b0407d30cf04fad2e064f3ed80e311a11
Nathan McGuire <nathan.e.mcguire@gmail.com>
2023-09-22 12:13:51 -0400

    Added various uints

--- lib/binary_parser/binary_parser.dart
+++ lib/binary_parser/binary_parser.dart
@@ -11,7 +11,15 @@ List parseData(ByteData data, String format) {
 
     switch (character) {
       case 'c':
+        output.add(String.fromCharCode(data.getUint8(index)));
+        index += 1;
+        break;
+      case 'b':
         output.add(data.getInt8(index));
+        index += 1;
+        break;
+      case 'B':A
+        output.add(data.getUint8(index));
         index += 1;
         break;
       case 'f':
