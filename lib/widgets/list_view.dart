2408561a2118e8f71905f2de1862387962143bf7
Matt <matthew.morley.ca@gmail.com>
2023-09-22 18:12:51 -0400

    ooga booga

---
+++ lib/widgets/list_view.dart
@@ -0,0 +1,36 @@
+import 'dart:collection';
+
+import 'package:flutter/material.dart';
+
+// TODO -- have database keep these around and mutate them, and have widgets hold them as part of their state
+class ListView<T> with ListMixin<T>, ChangeNotifier {
+  final List<T> _buf;
+  final int start, endPlusOne;
+
+  ListView(this.start, this.endPlusOne, this._buf);
+
+  @override
+  void add(T element) {
+    _buf.add(element);
+    notifyListeners();
+  }
+
+  @override
+  int get length => endPlusOne - start;
+
+  @override
+  T operator [](int index) {
+    return _buf[index + start];
+  }
+
+  @override
+  void operator []=(int index, T value) {
+    _buf[index] = value;
+    notifyListeners();
+  }
+
+  @override
+  set length(int newLength) {
+    throw UnsupportedError('Cannot resize a list view.');
+  }
+}
