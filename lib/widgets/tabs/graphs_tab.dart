871c2275f5e12e34e42d4f722125236181f6b964
Nathan McGuire <nathan.e.mcguire@gmail.com>
2023-09-26 15:03:20 -0400

    Made the graphs in the graphs tab point to the correct keys

--- lib/widgets/tabs/graphs_tab.dart
+++ lib/widgets/tabs/graphs_tab.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../graph_widget.dart';
+import '../../constants.dart';
 
 class GraphTab extends StatelessWidget {
   const GraphTab({super.key});
@@ -13,16 +14,17 @@ class GraphTab extends StatelessWidget {
 //      crossAxisSpacing: 10,
 //      mainAxisSpacing: 10,
       crossAxisCount: 3,
-      childAspectRatio: 1.8, //TODO: This displays really funny, IDK why
+      childAspectRatio: 1.8,
+      //TODO: This displays really funny, IDK why
       children: const <Widget>[
-        GraphWidget(title: "Altitude", keyList: []),
-        GraphWidget(title: "Speeds", keyList: []),
-        GraphWidget(title: "XYZ Accelerations", keyList: []),
-        GraphWidget(title: "RPY Orientation", keyList: []),
-        GraphWidget(title: "XYZ Rotational Velocities", keyList: []),
-        GraphWidget(title: "Magnetometer", keyList: []),
-        GraphWidget(title: "Pressure, Pressure, & Pressure", keyList: []),
-        GraphWidget(title: "State", keyList: []),
+        GraphWidget(title: "Altitude", keyList: [Constants.altitude, Constants.gpsAltitude]),
+        GraphWidget(title: "Speeds", keyList: [Constants.verticalSpeed]),
+        GraphWidget(title: "XYZ Accelerations", keyList: [Constants.accelerometerX, Constants.accelerometerY, Constants.accelerometerZ]),
+        GraphWidget(title: "RPY Orientation", keyList: [Constants.roll, Constants.pitch, Constants.yaw]),
+        GraphWidget(title: "XYZ Rotational Velocities", keyList: [Constants.gyroX, Constants.gyroY, Constants.gyroZ]),
+        GraphWidget(title: "Magnetometer", keyList: [Constants.magnetometerX, Constants.magnetometerY, Constants.magnetometerZ]),
+        GraphWidget(title: "Pressure, Pressure, & Pressure", keyList: [Constants.barometer1Pressure, Constants.barometer2Pressure, Constants.pressureReference]),
+        GraphWidget(title: "State", keyList: [Constants.fcbStateNumber]),
         GraphWidget(title: "RSSI", keyList: ["rssi"]),
       ],
     );
