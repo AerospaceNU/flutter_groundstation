d46fb5ec7b875fe5c8b3de627e528c8d4562be7c
alllison cross <67401718+allisoncross@users.noreply.github.com>
2023-09-27 22:03:35 -0400

    display markers on graph

--- lib/widgets/map_widget.dart
+++ lib/widgets/map_widget.dart
@@ -18,24 +18,28 @@ class MapWidget extends StatefulWidget {
   
   @override
   State<StatefulWidget> createState() => _MapWidgetState();
+
 }
 
+late List<LatLng> circles;
 
 
 
   
 class _MapWidgetState extends BaseWidgetState<MapWidget> {
-
+ 
 
   @override
   Widget build(BuildContext context) {
-    var lat = getDatabaseValue("latitude", 42.361);
-    var long = getDatabaseValue("longitude", -71.057);
+    var lat = getDatabaseValue("latitude", 42.361145);
+    var lon = getDatabaseValue("longitude", -71.0570803);
+
+    var groundstation_lat = 42.35;
+    var groundstation_lon = -71.06;
     return FlutterMap(
       options: MapOptions(
-        center: LatLng(lat, long),
-        //center: LatLng(42.361145, -71.0570803),
-        zoom: 17,
+        center: LatLng(lat, lon),
+        zoom: 10,
       ),
       
       nonRotatedChildren: [
@@ -53,6 +57,20 @@ class _MapWidgetState extends BaseWidgetState<MapWidget> {
           urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
           userAgentPackageName: 'com.example.app',
         ), 
+        CircleLayer(
+                  circles: [
+                    CircleMarker(
+                      point: LatLng(lat, lon),
+                      radius: 8,
+                      color: Color.fromARGB(255, 133, 2, 255),
+                    ),
+                    CircleMarker(
+                      point: LatLng(groundstation_lat, groundstation_lon),
+                      radius: 8,
+                      color: Color.fromARGB(255, 255, 154, 2),
+                    )
+                  ],
+                ),
       ],
     );
   }
