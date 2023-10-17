1c4201a96fb63391157a5c45bec93e0b5a10d69d
Nathan McGuire <nathan.e.mcguire@gmail.com>
2023-10-16 21:23:04 -0400

    Fixes

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,6 +1,4 @@
 class Constants {
-  static const databaseUpdateKey = "__database_update__";
-
   // For Text Resizing Optimization of large Font Sizes
   static const List<double> stdFontSizes =
   [1536, 1292, 1086, 914, 768, 646, 456, 322, 272, 228, 192, 162, 136, 114,
