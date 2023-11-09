a9b968fdb731af2df94562ac3f853ddb23767c15
Meghna Gite <meghnagite@gmail.com>
2023-11-08 21:15:01 -0500

    pivoting to streambuilder?

--- lib/widgets/stream_widget.dart
+++ lib/widgets/stream_widget.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'base_widget.dart';
+import 'dart:async';
 
 class StreamWidget extends StatefulWidget {
   const StreamWidget({super.key});
@@ -13,15 +14,34 @@ class StreamWidget extends StatefulWidget {
 class _StreamWidgetState extends BaseWidgetState<StreamWidget> {
   @override
   Widget build(BuildContext context) {
-    var textWidget = const Text('Stream widget time');
-    var wid_2 = const Text('You have pushed the button this many times:');
-    var wid_3 = Text('${getDatabaseValue("counter", 0)}',
-        style: Theme.of(context).textTheme.headlineMedium);
-    var wid_4 = TextButton(onPressed: () => {}, child: const Text("hello"));
+    List<String> importantFields = ["groundTemperature"];
+
+    Stream<int> getNumberStream() {
+      return Stream.periodic(Duration(seconds: 1), (int count) {
+        return count;
+      });
+    }
 
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[Text("It's streaming time")],
+      children: <Widget>[
+        Text("It's streaming time"),
+        StreamBuilder<int>(
+          stream: getNumberStream(),
+          builder: (BuildContext context, AsyncSnapshot<int> snapshot) {
+            if (snapshot.hasData) {
+              // Display the data from the stream
+              return Text('Number: ${snapshot.data}');
+            } else if (snapshot.hasError) {
+              // Handle error case
+              return Text('Error: ${snapshot.error}');
+            } else {
+              // Handle loading or initial state
+              return CircularProgressIndicator();
+            }
+          },
+        )
+      ],
     );
   }
 }
