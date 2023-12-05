57c8d91f5df811fd042818c888584a62f70f7b4b
Nathan McGuire <nathan.e.mcguire@gmail.com>
2023-12-04 21:05:15 -0500

    Diagnostics (#10)
    * pyro continuity widget
    
    * pyro widget
    
    * Implement continuity loop
    
    * trying to make draggable widget
    
    * changes?
    
    * fixes
    
    * making sample data work
    
    * updates
    
    * cleanup and tweaks prior to merge
    
    * minor formatting tweaks
    
    ---------
    
    Co-authored-by: adescoteaux1 <ally.descoteaux.0@gmail.com>

---
+++ lib/widgets/pyro_data_widget.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'base_widget.dart';
+
+class PyroDataWidget extends StatefulWidget {
+  const PyroDataWidget({super.key});
+
+  @override
+  State<PyroDataWidget> createState() => _PyroWidgetState();
+}
+
+class _PyroWidgetState extends BaseWidgetState<PyroDataWidget> {
+  _PyroWidgetState();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16.0),
+      height: 600,
+      child: Column(children: <Widget>[
+        Container(
+          height: 75,
+          decoration: BoxDecoration(border: Border.all(color: Colors.black)),
+          child: const Center(
+            child: Text('Pyro Output Data'),
+          ),
+        ),
+        SingleChildScrollView(
+          child: Container(
+            height: 400,
+            decoration: BoxDecoration(color: Colors.black, border: Border.all(color: Colors.black)),
+            child: const Center(
+              child: Text(
+                '1',
+                style: TextStyle(color: Colors.white),
+                textAlign: TextAlign.right,
+              ),
+            ),
+          ),
+        ),
+      ]),
+    );
+  }
+}
---
+++ lib/widgets/tabs/diagnostics_tab.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import '../pyro_continuity_widget.dart';
+import '../pyro_data_widget.dart';
+
+import 'package:flutter/material.dart';
+
+class DiagnosticsTab extends StatelessWidget {
+  const DiagnosticsTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.count(primary: false, padding: const EdgeInsets.all(20), crossAxisSpacing: 10, mainAxisSpacing: 10, crossAxisCount: 2, children: const <Widget>[
+      PyroContinuityWidget(),
+      PyroDataWidget(),
+    ]);
+  }
+}
