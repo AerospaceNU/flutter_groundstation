25f8cada63de3ccae4bae69b34bf907d94a1af91
elyssa-a-adams <elyssaa1995@gmail.com>
2024-01-22 20:15:09 -0500

    feat: stream widget changes (needs file picker)

--- lib/widgets/stream_widget.dart
+++ lib/widgets/stream_widget.dart
@@ -1,8 +1,12 @@
 // ignore_for_file: unused_local_variable
 
+import 'dart:convert';
+import 'dart:io';
+import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'base_widget.dart';
 import 'dart:async';
+import 'package:path_provider/path_provider.dart';
 
 class StreamWidget extends StatefulWidget {
   const StreamWidget({super.key});
@@ -11,7 +15,14 @@ class StreamWidget extends StatefulWidget {
   State<StreamWidget> createState() => _StreamWidgetState();
 }
 
+Future<String> get _localPath async {
+      final directory = await getApplicationDocumentsDirectory();
+
+      return directory.path;
+    }
+
 class _StreamWidgetState extends BaseWidgetState<StreamWidget> {
+  
   @override
   Widget build(BuildContext context) {
     List<String> importantFields = ["groundTemperature"];
@@ -22,16 +33,28 @@ class _StreamWidgetState extends BaseWidgetState<StreamWidget> {
       });
     }
 
+    Stream<String> getGyroStream() {
+      final File file = new File("/Users/elyssaadams/Documents/sprucegoose-11-19-2-output-FCB-post.csv");
+
+      Stream<List> inputStream = file.openRead();
+
+      Stream<String> stringStream = inputStream
+        .transform(utf8.decoder)          // Decode bytes to UTF-8.
+        .transform(new LineSplitter());    // Convert stream to individual lines.
+      return stringStream;
+      }
+
+  
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
         Text("It's streaming time"),
-        StreamBuilder<int>(
-          stream: getNumberStream(),
-          builder: (BuildContext context, AsyncSnapshot<int> snapshot) {
+        StreamBuilder<String>(
+          stream: getGyroStream(),
+          builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
             if (snapshot.hasData) {
               // Display the data from the stream
-              return Text('Number: ${snapshot.data}');
+              return Text('Information: ${snapshot.data}');
             } else if (snapshot.hasError) {
               // Handle error case
               return Text('Error: ${snapshot.error}');
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,9 @@ import FlutterMacOS
 import Foundation
 
 import flutter_libserialport
+import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterLibserialportPlugin.register(with: registry.registrar(forPlugin: "FlutterLibserialportPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
